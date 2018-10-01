64f4f128b82027530b57f238bc8b1bb8d2ee31a2
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-10-01 17:19:42 +0800

    Initial work on react stuff

---
+++ napc/src/SidebarPage.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { Route, NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+class SidebarPage extends Component {
+  componentDidUpdate (prevProps) {
+    const { need } = this.props.match.params
+    if (prevProps.match.params.need !== need) {
+      console.log('Do something with need: ', need)
+    }
+  }
+  render () {
+    return (
+      <div className={this.props.className}>
+        <div className='overlay'>
+          Basic Need: {this.props.match.params.need}
+        </div>
+        <div className='sidebar'>
+          <NavLink activeClassName='active' to='/water'>Water</NavLink>
+          <NavLink activeClassName='active' to='/fire'>Fire</NavLink>
+          <NavLink activeClassName='active' to='/wind'>Wind</NavLink>
+          <NavLink activeClassName='active' to='/earth'>Earth</NavLink>
+          <NavLink activeClassName='active' to='/love'>Love</NavLink>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default styled(SidebarPage)`
+  .sidebar {
+    width: 50vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    right: 0;
+  }
+  .sidebar .active {
+    background: pink;
+  }
+`
