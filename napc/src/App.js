e1ca3c10f04ac3aafb8cd372f3774551df777186
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-10-01 17:19:42 +0800

    Initial work on react stuff

---
+++ napc/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+
+import SidebarPage from './SidebarPage'
+
+const Map = () => (
+  <div>I'm a map</div>
+)
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Map />
+        <Route exact path='/' component={SidebarPage} />
+        <Route exact path='/:need' component={SidebarPage} />
+      </div>
+    )
+  }
+}
+
+export default App
