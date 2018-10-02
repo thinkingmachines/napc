236fac21518f6e1b452fc8057df92a505530f720
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-10-02 11:12:46 +0800

    Switch from CRA to neutrino

--- napc/src/SidebarPage.js
+++ napc/src/SidebarPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 class SidebarPage extends Component {
--- napc/src/index.js
+++ napc/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
-registerServiceWorker()
+import App from './App'
+
+const root = document.getElementById('root')
+const load = () => render((
+  <AppContainer>
+    <BrowserRouter><App /></BrowserRouter>
+  </AppContainer>
+), root)
+
+// This is needed for Hot Module Replacement
+if (module.hot) {
+  module.hot.accept('./App', load)
+}
+
+load()
